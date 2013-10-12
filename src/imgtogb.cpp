e8a1ab13899de8611b6dc15484e7f3ab8697aad4
SimonLarsen <simonhffh@gmail.com>
2013-10-12 16:06:37 +0200

    Added default name from input file if none is given

--- src/imgtogb.cpp
+++ src/imgtogb.cpp
@@ -1,7 +1,7 @@
 #include <tclap/CmdLine.h>
-#include <string>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
 #include "Image.hpp"
 #include "Tile.hpp"
@@ -82,6 +82,23 @@ void produceTilemap(const Image& image, int offset, const std::string& name, std
 	os << "#endif" << std::endl;
 }
 
+/**
+ * Gets the basename of a path without the file ending.
+ * Given the path "/home/user/filename.png" the string "filename" is returned.
+ *
+ * @param path File path
+ * @return Name of file
+ */
+std::string getName(const std::string& path) {
+	size_t lastSlash = path.find_last_of('/');
+	lastSlash = (lastSlash == std::string::npos ? 0 : lastSlash+1);
+	size_t lastBSlash = path.find_last_of('\\');
+	lastBSlash = (lastBSlash == std::string::npos ? 0 : lastBSlash+1);
+	size_t end = path.find_last_of('.');
+	size_t start = std::max(lastSlash, lastBSlash);
+	return path.substr(start, end-start);
+}
+
 int main(int argc, char **argv) {
 	Image image;
 	std::string imageFilename, outputFilename, outputName;
@@ -100,7 +117,7 @@ int main(int argc, char **argv) {
 		// Value arguments
 		TCLAP::UnlabeledValueArg<std::string> imageFilenameArg("image","Image to convert", true, "", "IMAGE", cmd);
 		TCLAP::ValueArg<std::string> outputFilenameArg("o","out","Output file", false, "", "FILE", cmd);
-		TCLAP::ValueArg<std::string> outputNameArg("n","name","Output name", false, "default", "NAME", cmd);
+		TCLAP::ValueArg<std::string> outputNameArg("n","name","Output name", false, "", "NAME", cmd);
 		TCLAP::ValueArg<int> offsetArg("O","offset","Add offset to tile map indices", false, 0, "OFFSET", cmd);
 
 		// Sprite size switches
@@ -119,6 +136,11 @@ int main(int argc, char **argv) {
 
 	} catch (TCLAP::ArgException &e) {
 		std::cerr << "error: " << e.error() << "for arg " << e.argId() << std::endl;
+	}
+
+	// Get output name from file name if none is given.
+	if(outputName.length() == 0) {
+		outputName = getName(imageFilename);
 	}
 
 	// Load input image
--- src/imgtogb.hpp
+++ src/imgtogb.hpp
@@ -1,7 +1,10 @@
 #ifndef __IMGTOGB_HPP
 #define __IMGTOGB_HPP
 
+#include <string>
+
 void produceSprites(const Image& image, bool size8x16, const std::string& name, std::ostream& os);
 void produceTilemap(const Image& image, int offset, const std::string& name, std::ostream& os);
+std::string getName(const std::string& path);
 
 #endif
